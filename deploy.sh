d7ca8f94697c6ec4a2fa2188b4b82fa62a18387d
Toon Spin <toon.spin@gmail.com>
2024-02-18 22:28:06 +0100

    Debug actions

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,2 @@
-# Environment variables needed:
-# AWS_ACCESS_KEY_ID
-# AWS_SECRET_ACCESS_KEY
-# AWS_DEFAULT_REGION
-# CDN_DISTRIBUTION_ID
-# S3_BUCKET_NAME
-
-echo Syncing "dist" directory with S3 bucket...
-aws s3 sync --delete dist "s3://${S3_BUCKET_NAME}"
-
-echo Invalidating CloudFront distribution...
-aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"
+echo $CDN_DISTRIBUTION_ID
+echo $S3_BUCKET_NAME
